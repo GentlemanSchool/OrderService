539682a5a5eaca804270b17c75f06808c9833e41
Egor <egormalov.2007@gmail.com>
2026-03-16 18:38:10 +0300

    GP-26: repository added

---
+++ src/main/java/ru/gentleman/order/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package ru.gentleman.order.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.gentleman.order.entity.Order;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+}
