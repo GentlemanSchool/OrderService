9cd39ce2a20f6e0ba323bee1b76a90995d84b1c2
Egor <egormalov.2007@gmail.com>
2026-03-16 21:06:12 +0300

    GP-27: queries added and handler added

---
+++ src/main/java/ru/gentleman/order/query/handler/OrderHandler.java
@@ -0,0 +1,28 @@
+package ru.gentleman.order.query.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+import ru.gentleman.order.dto.OrderDto;
+import ru.gentleman.order.query.FindAllOrdersByUserIdQuery;
+import ru.gentleman.order.query.FindOrderByIdQuery;
+import ru.gentleman.order.service.OrderService;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class OrderHandler {
+
+    private final OrderService orderService;
+
+    @QueryHandler
+    public OrderDto on(FindOrderByIdQuery query) {
+        return this.orderService.get(query.id());
+    }
+
+    @QueryHandler
+    public List<OrderDto> on(FindAllOrdersByUserIdQuery query) {
+        return this.orderService.getAllByUserId(query.userId());
+    }
+}
